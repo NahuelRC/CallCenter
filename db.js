d998bef08344c968b3df16bf56ca0621acf65870
nmurugasolvd <nmuruga@solvd.com>
2025-07-23 23:05:04 -0300

    Fix el warning de mongo

--- db.js
+++ db.js
@@ -8,10 +8,7 @@ const mongoURI = process.env.MONGODB_URI || 'mongodb://localhost:27017/herbalis'
 export const conectarDB = async () => {
    if (mongoose.connection.readyState >= 1) return;
   try {
-    await mongoose.connect(mongoURI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
+    await mongoose.connect(mongoURI);
     console.log('✅ Conectado a MongoDB');
   } catch (error) {
     console.error('❌ Error conectando a MongoDB:', error.message);
