aa2aaa64aaa865936616a97dcf67998e2bf2718f
nmurugasolvd <nmuruga@solvd.com>
2025-08-05 22:35:33 -0300

    Modifique para tener un prompt activo

--- models/Prompt.js
+++ models/Prompt.js
@@ -1,9 +1,10 @@
 import mongoose from 'mongoose';
 
 const promptSchema = new mongoose.Schema({
- 
+  nombre: { type: String, required: true },
   content: { type: String, required: true },
-  creadoEn: { type: Date, default: Date.now }
+  creadoEn: { type: Date, default: Date.now },
+  activo: { type: Boolean, default: false }
 });
 
 const Prompt = mongoose.models.Prompt || mongoose.model('Prompt', promptSchema);
